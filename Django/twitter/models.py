f15fda53755e0814d60e3684c594490476505ec2
Kaja Machalska <machal.kaja@gmail.com>
2019-06-30 17:36:57 +0200

    first model done

--- Django/twitter/models.py
+++ Django/twitter/models.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    creation_date = models.DateTimeField(default=datetime.now)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
